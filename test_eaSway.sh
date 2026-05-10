318fce72f20344ea9101d3c230bf68988c21d796
dozelix <wsamuelixyw@gmail.com>
2026-05-10 11:46:44 -0400

    finalizar infraestructura de sandbox y permisos de hardware v0.0.3 (#12)
    - Actualizado test_eaSway.sh con parámetros avanzados de ejecución para soporte gráfico.
    - Añadido mapeo de dispositivos de entrada (/dev/input) para permitir interactividad en el contenedor.
    - Implementado montaje del socket de Wayland ($WAYLAND_DISPLAY) para habilitar sesiones 'nested'.
    - Configurada la exportación de variables de entorno críticas (XDG_RUNTIME_DIR, DISPLAY) para el puente host-container.
    - Finalizada la Tarea #03 del Backlog: Corrección de permisos de TTY y DRM.

--- test_eaSway.sh
+++ test_eaSway.sh
@@ -36,10 +36,16 @@ echo " XDG_RUNTIME_DIR:  $RUNTIME_DIR"
 echo "-----------------------------------------------------------"
 
 # Ejecución ajustada al hardware Intel y sincronización de volumen (Tarea #01)
-docker run -it \
-    --privileged \
+
+docker run -it --privileged \
     --name "$CONTAINER_NAME" \
+    --network=host \
     --device /dev/dri:/dev/dri \
+    --device /dev/input:/dev/input \
     -e XDG_RUNTIME_DIR="$RUNTIME_DIR" \
+    -e WAYLAND_DISPLAY="$WAYLAND_DISPLAY" \
+    -e DISPLAY="$DISPLAY" \
+    -e XDG_SESSION_TYPE="wayland" \
+    -v "$XDG_RUNTIME_DIR/$WAYLAND_DISPLAY:$RUNTIME_DIR/$WAYLAND_DISPLAY" \
     -v "$(pwd):/home/$HOST_USER/eaSway" \
     "$IMAGE_NAME" /bin/bash
